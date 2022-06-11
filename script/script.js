0095c0b35829c58f6d92cf98ab583a6dab5faefa
zZMarza <corrado.marzagalli@gmail.com>
2022-06-11 18:40:31 +0200

    one round rock paper scissor completed

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 function computerPlay(){
     let computer_choice = Array('rock','paper','scissor');
-    document.getElementById("demo2").innerHTML = computer_choice;
-    return computer_choice[Math.floor(Math.random()*computer_choice.length)];
+    computer_choice = computer_choice[Math.floor(Math.random()*computer_choice.length)];
+    document.getElementById("computer-choice").innerHTML = 'computer choice: '+computer_choice;
+    return computer_choice;
 }
 
 function playerPlay() {
@@ -13,7 +14,7 @@ function playerPlay() {
 
         if(player_choice == 'rock' || player_choice == 'paper' || player_choice == 'scissor')
         {
-            document.getElementById("demo").innerHTML = player_choice;                
+            document.getElementById("player-choice").innerHTML = 'player choice: '+player_choice;;                
         }
         else
         {
@@ -29,9 +30,27 @@ function playerPlay() {
     return player_choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    
+    let match_result;
 
-let computerSelection = computerPlay();
+    if((playerSelection == 'rock' && computerSelection == 'scissor') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissor' && computerSelection == 'paper'))
+    {
+        match_result = 'you won';
+    }
+    else if(playerSelection === computerSelection)
+    {
+        match_result = 'draw';
+    }
+    else
+    {
+        match_result = 'you lost';
+    }
+    
+    document.getElementById("match-result").innerHTML = match_result;                
+    return match_result;
+}  
 
-let playerSelection = playerPlay();
-
-
+let player_choice = playerPlay();
+let computer_choice = computerPlay();
+let match_result = playRound(player_choice, computer_choice);
