38abefc5a5725eb7266b300dfca440b0c0619d91
zZMarza <corrado.marzagalli@gmail.com>
2022-06-11 15:55:10 +0200

    player selection defined

--- script/script.js
+++ script/script.js
@@ -1,8 +1,37 @@
 function computerPlay(){
-    let items = Array('rock','paper','scissor');
-    return items[Math.floor(Math.random()*items.length)];
+    let computer_choice = Array('rock','paper','scissor');
+    document.getElementById("demo2").innerHTML = computer_choice;
+    return computer_choice[Math.floor(Math.random()*computer_choice.length)];
 }
 
+function playerPlay() {
+    let player_choice = prompt("Rock, Paper or Scissor?");
+
+    if (player_choice != null) 
+    {
+        player_choice = player_choice.toLowerCase();  
+
+        if(player_choice == 'rock' || player_choice == 'paper' || player_choice == 'scissor')
+        {
+            document.getElementById("demo").innerHTML = player_choice;                
+        }
+        else
+        {
+            console.log('choice not valid');
+        }
+    }
+    
+    else
+    {
+        console.log('choice not valid');
+    }
+
+    return player_choice;
+}
+
+
 let computerSelection = computerPlay();
+
+let playerSelection = playerPlay();
 
 
